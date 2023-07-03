d860b52a633703e1aad9a0b7d2978fe85f872e1b
Dhafa <dhofaaryanda4@gmail.com>
2023-07-03 17:50:55 +0700

    complete todo 2

--- app/src/main/java/com/dicoding/todoapp/data/Task.kt
+++ app/src/main/java/com/dicoding/todoapp/data/Task.kt
@@ -6,7 +6,6 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 //TODO 1 : Define a local database table using the schema in app/schema/tasks.json
-
 @Entity(tableName = "tasks")
 data class Task(
     @PrimaryKey(autoGenerate = true)
